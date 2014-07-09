ff88d33bfba19bee4b6d76cf79e84ddd18cc822b
tlabean <labean112@gmail.com>
2014-07-09 15:52:50 -0400

    Created bike with rotating wheels

---
+++ Main/Assets/Scripts/BikeController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeController : MonoBehaviour {
+
+	public GradeController gradeController;
+	public float fluidFriction;
+	public float pedalTorque;
+	public float bikeMass;
+	public float gearMultiplier;
+	public int gear;
+
+	public Transform frontWheel;
+	public Transform backWheel;
+	public float wheelRadius;
+	public Transform pedals;
+
+	public float velocity;
+
+	void Awake()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		velocity -= velocity * velocity * fluidFriction / bikeMass;
+
+		frontWheel.Rotate ( velocity * Time.deltaTime / wheelRadius / 2f * Mathf.PI * Mathf.Rad2Deg, 0f, 0f);
+		backWheel.Rotate ( velocity * Time.deltaTime / wheelRadius / 2f * Mathf.PI * Mathf.Rad2Deg, 0f, 0f);
+		pedals.Rotate ( velocity * Time.deltaTime / wheelRadius / 2f * Mathf.PI * Mathf.Rad2Deg / ((float)(gear-1) * gearMultiplier + 1f) , 0f, 0f);
+
+	}
+
+}
---
+++ Main/Assets/Scripts/GradeController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GradeController : MonoBehaviour {
+
+	public float grade = 0f;
+	public GameObject ground;
+	public BikeController bikeController;
+
+	void FixedUpdate()
+	{
+		//ground.transform.position = ground.transform.position + Vector3.forward * bikeController.velocity * Time.deltaTime;
+
+		Material groundMaterial = ground.GetComponent<MeshRenderer>().material;
+		groundMaterial.mainTextureOffset = new Vector2(0f, groundMaterial.mainTextureOffset.y + bikeController.velocity * Time.deltaTime);
+	}
+}
