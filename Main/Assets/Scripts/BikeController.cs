915f9c9f49770d295aa582a38ab53a4fdd78cae9
tlabean <labean112@gmail.com>
2014-07-10 10:49:08 -0400

    Added physics pedaling and gear changing
    up and down arrows to change gears

--- Main/Assets/Scripts/BikeController.cs
+++ Main/Assets/Scripts/BikeController.cs
@@ -24,8 +24,18 @@ public class BikeController : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		velocity -= velocity * velocity * fluidFriction / bikeMass;
 
+		if(Input.GetKeyDown(KeyCode.UpArrow) && gear < 9)
+			gear++;
+		if(Input.GetKeyDown(KeyCode.DownArrow) && gear > 1)
+			gear--;
+
+
+		velocity -= (velocity * fluidFriction + velocity * velocity * fluidFriction) / bikeMass * Time.deltaTime;
+		velocity += -9.8f * Mathf.Sin (gradeController.grade * Mathf.Deg2Rad) * Time.deltaTime;
+		velocity += pedalTorque / ((float)(gear - 1) * gearMultiplier + 1f) * Time.deltaTime;
+		if(velocity < 0f)
+			velocity = 0f;
 		frontWheel.Rotate ( velocity * Time.deltaTime / wheelRadius / 2f * Mathf.PI * Mathf.Rad2Deg, 0f, 0f);
 		backWheel.Rotate ( velocity * Time.deltaTime / wheelRadius / 2f * Mathf.PI * Mathf.Rad2Deg, 0f, 0f);
 		pedals.Rotate ( velocity * Time.deltaTime / wheelRadius / 2f * Mathf.PI * Mathf.Rad2Deg / ((float)(gear-1) * gearMultiplier + 1f) , 0f, 0f);
