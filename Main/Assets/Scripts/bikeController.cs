deee67cbf882126de354e6cde998b13e0b8e638f
creed30 <creed30@gatech.edu>
2014-07-13 21:49:16 -0400

    Made it Move!

--- Main/Assets/Scripts/bikeController.cs
+++ Main/Assets/Scripts/bikeController.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class bikeController : MonoBehaviour {
 
+
+public class bikeController : MonoBehaviour {
+	public float SPEED_CONSTANT = 0.5f;
+	
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z+SPEED_CONSTANT);
+
 	}
 }
