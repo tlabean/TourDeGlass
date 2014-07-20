402ace0120e7bcf38ef0c863a093354a16f963be
creed30 <creed30@gatech.edu>
2014-07-20 13:03:47 -0400

    Metronome Ambient

---
+++ Main/Assets/Scripts/AmbientChannel.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientChannel : MonoBehaviour {
+
+	public BikeController bikeController;
+	public GradeController gradeController;
+	public AudioClip ambientSound;
+	private AudioSource audioSource;
+	public float cadenceTarget = 7f;
+	// Use this for initialization
+	void Start () {
+		InitializeAudioSource();
+		audioSource.Play ();
+		audioSource.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gradeController.grade > 3) {
+			audioSource.volume = Mathf.Abs(1 - (Mathf.Abs(bikeController.pedalCadence / cadenceTarget)));
+			//audioSource.pitch = Mathf.Abs(1 - (Mathf.Abs(bikeController.pedalCadence / cadenceTarget)));
+			audioSource.pitch = 2f;
+		}
+		else if ( gradeController.grade < -3) {
+			audioSource.volume = Mathf.Abs(1 - (Mathf.Abs(bikeController.pedalCadence / cadenceTarget)));
+			//audioSource.pitch = Mathf.Abs(1 - (Mathf.Abs(bikeController.pedalCadence / cadenceTarget)));
+			audioSource.pitch = 1f;
+		}
+		else {
+			audioSource.volume = 0;
+
+		}
+
+	}
+
+	void InitializeAudioSource()
+	{
+		//we setup a new AudioSource here to not conflict with any ambient sound effects
+		//we may have put on objects in our scene
+		
+		//This lets us have an ambient loop play while also playing a different
+		//sound on collision with the character or another object.
+		
+		if (audioSource != null) //if we have already set it up
+			return;
+		GameObject go = new GameObject();
+		go.transform.parent = transform;
+		go.name = "Amibent-" + gameObject.name;
+		audioSource = go.AddComponent<AudioSource>();
+		audioSource.clip = ambientSound;
+	}
+}
