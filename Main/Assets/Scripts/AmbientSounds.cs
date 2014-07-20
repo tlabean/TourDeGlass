f13df3f9fe576766e4ec0f1843b447c228f78109
creed30 <creed30@gatech.edu>
2014-07-20 16:42:48 -0400

    added the bicycle sound and controller thingy

---
+++ Main/Assets/Scripts/AmbientSounds.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientSounds : MonoBehaviour {
+	public AudioSource bicycleAmbience;
+	public BikeController bikeController;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		bicycleAmbience.volume = (bikeController.velocity / 100f);
+	}
+}
