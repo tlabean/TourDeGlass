537fe5487434719f77d8008e78eb99f2e6e76375
tlabean <labean112@gmail.com>
2014-07-20 20:52:26 -0400

    started GUI

---
+++ Main/Assets/Scripts/GUIController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+
+	public BikeController bikeController;
+	public GradeController gradeController;
+
+	void OnGUI()
+	{
+
+		string gearMessage = "";
+
+		if(bikeController.pedalCadence > 7.5f)
+		{
+			gearMessage = "UPSHIFT";
+		}
+		else if(bikeController.pedalCadence < 6.5)
+		{
+			gearMessage = "DOWNSHIFT";
+		}
+		else
+		{
+			gearMessage = "PERFECT";
+		}
+
+		GUI.Box(new Rect(Screen.width * 0.6f, 0f, Screen.width * 0.4f, Screen.height * 0.3f), "<size=50>Incline: " + gradeController.grade + "\n" + gearMessage + "</size>");
+	}
+}
