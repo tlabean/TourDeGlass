9abf8c4fc533a5130cb296756a4c5dca33d69775
creed30 <creed30@gatech.edu>
2014-07-20 21:50:12 -0400

    added alert noise

---
+++ Main/Assets/Scripts/AlertNotify.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlertNotify : MonoBehaviour {
+
+	public AudioSource alertSound;
+	public BikeController bikeController;
+	private bool flagSet = false;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		if (bikeController.pedalCadence > 10 && !alertSound.isPlaying && !flagSet) {
+						alertSound.volume = 0.25f;
+						alertSound.Play ();
+						flagSet = true;
+				}
+		if (bikeController.pedalCadence < 10 && flagSet) {
+						flagSet = false;
+						alertSound.volume = 0;
+				}
+
+
+	}
+}
